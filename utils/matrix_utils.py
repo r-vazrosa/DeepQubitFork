349ee5573c700070dba2127fc419fb234b91f660
ian-turner <i.r.turner55@gmail.com>
2025-03-30 16:36:44 -0400

    aligning unitaries during nnet conversion

--- utils/matrix_utils.py
+++ utils/matrix_utils.py
@@ -24,21 +24,6 @@ def tensor_product(mats: List[np.ndarray[np.complex128]]) -> np.ndarray[np.compl
     return current
 
 
-def unitary_to_nnet_input(unitary: np.ndarray[np.complex128]) -> np.ndarray[float]:
-    """
-    Converts a complex-valued unitary matrix into real-valued
-    flat numpy arrays that can be converted to tensors easily
-
-    @param unitary: Unitary matrix to convert
-    @returns: Numpy vector of real and imaginary values of matrix
-    """
-    unitary_flat = unitary.flatten()
-    unitary_real = np.real(unitary_flat)
-    unitary_imag = np.imag(unitary_flat)
-    unitary_nnet = np.hstack((unitary_real, unitary_imag)).astype(float)
-    return unitary_nnet
-
-
 def phase_align(unitary: np.ndarray[np.complex128]) -> np.ndarray[np.complex128]:
     """
     Aligns the global phase of a unitary so that the top left
@@ -49,6 +34,22 @@ def phase_align(unitary: np.ndarray[np.complex128]) -> np.ndarray[np.complex128]
     """
     phs: float = np.angle(unitary[0][0])
     return np.exp(-1j * phs) * unitary
+
+
+def unitary_to_nnet_input(unitary: np.ndarray[np.complex128]) -> np.ndarray[float]:
+    """
+    Converts a complex-valued unitary matrix into real-valued
+    flat numpy arrays that can be converted to tensors easily
+
+    @param unitary: Unitary matrix to convert
+    @returns: Numpy vector of real and imaginary values of matrix
+    """
+    unitary_aligned = phase_align(unitary)
+    unitary_flat = unitary_aligned.flatten()
+    unitary_real = np.real(unitary_flat)
+    unitary_imag = np.imag(unitary_flat)
+    unitary_nnet = np.hstack((unitary_real, unitary_imag)).astype(float)
+    return unitary_nnet
 
 
 def mats_close(mat1: np.ndarray[np.complex128], mat2: np.ndarray[np.complex128], epsilon: float) -> bool:
