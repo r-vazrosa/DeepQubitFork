33572c9912ab6139377fb148d7e5d9d5ae6115a0
ian-turner <i.r.turner55@gmail.com>
2025-07-17 15:44:07 -0400

    removed unused nnet

--- utils/pytorch_models.py
+++ utils/pytorch_models.py
@@ -130,25 +130,3 @@ class ResnetModel(HeurFnNNet):
         # output
         x = self.fc_out(x)
         return x
-
-
-class QNNet(HeurFnNNet):
-    def __init__(self, input_size: int, resnet_dim: int, num_resnet_blocks: int, \
-                 fc_input_dim: int, fc_layer_dims: List[int]):
-        super(QNNet, self).__init__(nnet_type='V')
-        
-        self.fc_input = nn.Linear(input_size, resnet_dim)
-        self.resnet = ResnetModel(resnet_dim, num_resnet_blocks, fc_input_dim, batch_norm=False)
-        self.fully_connected = FullyConnectedModel(
-            fc_input_dim,
-            fc_layer_dims,
-            layer_batch_norms=[False] * len(fc_layer_dims),
-            layer_acts=['RELU'] * len(fc_layer_dims),
-        )
-    
-    def forward(self, states_goals_l: List[torch.Tensor]) -> torch.Tensor:
-        x: torch.Tensor = states_goals_l[0].float()
-        x = self.fc_input(x)
-        x = self.resnet(x)
-        x = self.fully_connected(x)
-        return x
